fe9fa8d55f03d18d156d7a31a243c86e808125fb
Medina <ammedina@inf.ufsm.br>
2021-08-10 09:50:38 -0300

    test: use local file cache repository to store access token

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,21 @@
 
 namespace Jetimob\PortoSeguro\Tests;
 
+use Jetimob\Http\Authorization\OAuth\Storage\FileCacheRepository;
+use Jetimob\PortoSeguro\Facades\PortoSeguro;
 use Jetimob\PortoSeguro\PortoSeguroServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        PortoSeguro::getHttpInstance()->overwriteConfig(
+            'oauth_access_token_repository',
+            FileCacheRepository::class,
+        );
+    }
+
     /** @inheritDoc */
     protected function getPackageProviders($app)
     {
